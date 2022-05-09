a9f5c5164a49335064f04c6ab7dfd990fbed6471
e.nigma <60445182+BeBeeMan@users.noreply.github.com>
2022-05-09 21:30:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,39 +6,39 @@ int main()
 {
     string alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string done = "";
-	int c = 0;
+    int c = 0;
 
-	while (getline(cin, done))
-	{
-		for (int j = 0; j < done.length(); j++)
-		{
-			c = 0;
-			for (int i = 0; i < alfabet.length(); i++)
-			{
-				if (done[j] == ' ')
-				{
-					cout << ' ';
-					break;
-				}
-				else if (done[j] != alfabet[i])
-				{
-					c++;
-				}
-				else
-				{
-					if (c + 3 > alfabet.length()-1)
-					{
-						cout << alfabet[(c+3)-alfabet.length()];
-					}
-					else
-					{
-						cout << alfabet[c + 3];
-					}
-				}
-			}
-		}
-		cout << endl;
-	}
+    while (getline(cin, done))
+    {
+	    for (int j = 0; j < done.length(); j++)
+	    {
+		   c = 0;
+		   for (int i = 0; i < alfabet.length(); i++)
+		    {
+			    if (done[j] == ' ')
+			    {
+			    	cout << ' ';
+				break;
+		   	    }
+			    else if (done[j] != alfabet[i])
+			    {
+				    c++;
+			    }
+			    else
+			    {
+				    if (c + 3 > alfabet.length()-1)
+				    {
+					    cout << alfabet[(c+3)-alfabet.length()];
+				    }
+				    else
+				    {
+					    cout << alfabet[c + 3];
+				    }
+			    }
+		    }
+	    }
+	    cout << endl;
+    }
 }
 
 //https://pl.spoj.com/problems/JSZYCER/
